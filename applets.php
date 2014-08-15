263411db96ace4a858695302db3a7dbd25f702ba
Manoj Dayaram <noj@moovweb.com>
2014-08-15 02:00:37 -0700

    woops, need to update the links

--- applets.php
+++ applets.php
@@ -12,45 +12,45 @@ include('./src/headTemplate.inc');
 <p>Here are some applets.  All are made by either me or Will Battistelli.  In a selective few, both of us worked on them.  Anyways.  Have your fun with them.  Most of them are very incomplete as you can see, and some may even have a few bugs.  Source code is available though, so if you feel like you can improve, just <a href="contact.php">email me</a> the new <b>.java</b> files.  Credit will be given.</p>
 
 <p>
-<b>Applet:</b>  <a href="/old/applets/PokerApplet.html" target="Poker" onClick="window.open(this.href,this.target,'width=670,height=455')">Poker: 5 Card Draw</a><br>
+<b>Applet:</b>  <a href="applets/PokerApplet.html" target="Poker" onClick="window.open(this.href,this.target,'width=670,height=455')">Poker: 5 Card Draw</a><br>
 <b>Description:</b>  Simple game of five card draw.  If you notice, it's missing betting.  And you know, since it didn't have betting, we figured it could lack a score keeping system as well.  Out of all the applets, this is probably one which could use the most improvement.  To play follow the simple order of buttons:  Deal, Discard, End Turn, Show, and repeat.  I'm sure there's a few bugs in there that could use patching too.  But hey, at least the pictures are cool.*<br>
-<b>Source:</b>  <a href="/old/applets/PokerApplet.zip">PokerApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/PokerApplet.zip">PokerApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <p>
-<b>Applet:</b>  <a href="/old/applets/BinaryClockApplet.html" target="Binary Clock" onClick="window.open(this.href,this.target,'width=220,height=163')">Binary Clock</a><br>
+<b>Applet:</b>  <a href="applets/BinaryClockApplet.html" target="Binary Clock" onClick="window.open(this.href,this.target,'width=220,height=163')">Binary Clock</a><br>
 <b>Description:</b>  Nice little binary clock.  Even shows seconds, though I doubt that feature is going to get much use.<br>
-<b>Source:</b>  <a href="/old/applets/BinaryClockApplet.zip">BinaryClockApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/BinaryClockApplet.zip">BinaryClockApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <p>
-<b>Applet:</b>  <a href="/old/applets/NumberGuessApplet.html" target="Number Guess" onClick="window.open(this.href,this.target,'width=420,height=170')">Number Guessing Game</a><br>
+<b>Applet:</b>  <a href="applets/NumberGuessApplet.html" target="Number Guess" onClick="window.open(this.href,this.target,'width=420,height=170')">Number Guessing Game</a><br>
 <b>Description:</b>  Computer picks a random number between 1 and 100 and you guess what it is.  I guess we should've added some counter to keep track of the number of guesses.  But hey, then what would you fix?<br>
-<b>Source:</b>  <a href="/old/applets/NumberGuessApplet.zip">NumberGuessApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/NumberGuessApplet.zip">NumberGuessApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <p>
-<b>Applet:</b>  <a href="/old/applets/CrapsApplet.html" target="Casino Craps" onClick="window.open(this.href,this.target,'width=365,height=180')">Craps Dice Game</a><br>
+<b>Applet:</b>  <a href="applets/CrapsApplet.html" target="Casino Craps" onClick="window.open(this.href,this.target,'width=365,height=180')">Craps Dice Game</a><br>
 <b>Description:</b>  Game of craps.  You roll the dice.  If you get 7 or 11 off the bat you win.  If not, whatever number you get your goal is to get on the next roll.  I think you lose if you get snake eyes, not sure.  I never did understand the game all that well.<br>
-<b>Source:</b>  <a href="/old/applets/CrapsApplet.zip">CrapsApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/CrapsApplet.zip">CrapsApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <p>
-<b>Applet:</b>  <a href="/old/applets/SatApplet.html" target="Vocab Test" onClick="javascript:window.open(this.href,this.target,'width=570,height=620')">Vocab Test</a><br>
+<b>Applet:</b>  <a href="applets/SatApplet.html" target="Vocab Test" onClick="javascript:window.open(this.href,this.target,'width=570,height=620')">Vocab Test</a><br>
 <b>Description:</b>  Pick the number of questions you want and the number of answer choices.  Score ranks from zero (get everything wrong) to 200 (leave everything blank) to 800 (all correct).  You can go back and change your answers.  It's not case sensitive.  Overall, I think this is pretty good, but the layout sucks.  My only experience with layouts is FlowLayout...that should probably explain it.<br>
-<b>Source:</b>  <a href="/old/applets/SatApplet.zip">SatApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/SatApplet.zip">SatApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <p>
-<b>Applet:</b>  <a href="/old/applets/TicApplet.html" target="Tic Tac Toe" onClick="window.open(this.href,this.target,'width=220,height=170')">Tic Tac Toe</a><br>
+<b>Applet:</b>  <a href="applets/TicApplet.html" target="Tic Tac Toe" onClick="window.open(this.href,this.target,'width=220,height=170')">Tic Tac Toe</a><br>
 <b>Description:</b>  Not much to say here.  Game of tic tac toe, and what's this? *gasp* as score keeping system!  I'm not brain dead after all!<br>
-<b>Source:</b>  <a href="/old/applets/TicApplet.zip">TicApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/TicApplet.zip">TicApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <p>
-<b>Applet:</b>  <a href="/old/applets/CalculatorApplet.html" target="Calculator" onClick="window.open(this.href,this.target,'width=370,height=270')">Simple Calculator</a><br>
+<b>Applet:</b>  <a href="applets/CalculatorApplet.html" target="Calculator" onClick="window.open(this.href,this.target,'width=370,height=270')">Simple Calculator</a><br>
 <b>Description:</b>  I love this thing.  Probably the simplest calculator out there.  It rounds to three decimal places on division, power, and root operations.  Not sure if it rounds in multiplication, and as you guessed, I'm too lazy to check.  Either way, enjoy.<br>
-<b>Source:</b>  <a href="/old/applets/CalculatorApplet.zip">CalculatorApplet.zip</a><br>
+<b>Source:</b>  <a href="applets/CalculatorApplet.zip">CalculatorApplet.zip</a><br>
 <b>Modifications:</b>  6/24/05 - None
 </p>
 <br><br>
