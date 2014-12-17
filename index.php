f8ac48e4641ead91a71b26e96bb9299cca4a99fe
Manoj Dayaram <noj@moovweb.com>
2014-12-17 02:06:15 -0800

    fixing bullets

--- index.php
+++ index.php
@@ -38,16 +38,16 @@ A nice little random update.  Nothing new really on the site, except maybe the <
 <br><br>
 <h3> June 26, 2005</h3>
 It never seizes to amaze me.  Screwed for a fourth time.<br>
-• <a href="http://www.tampabays10.com/news/news.aspx?storyid=15280" target="brothel">$1 Billion bail</a>
+&#8226; <a href="http://www.tampabays10.com/news/news.aspx?storyid=15280" target="brothel">$1 Billion bail</a>
 <br><br>
 Bye 8th Amendment...we'll miss you!
 <br><br>
 <h3> June 23, 2005</h3>
 Raped by the Government v3.0!<br>
 <i>Updates</i><br>
-• <a href="http://www.boingboing.net/2005/06/22/rottencom_our_gaping.html" target="nomorepr0n">US Code: Title 18: Section 2257</a><br>
-• <a href="http://www.washingtonpost.com/wp-dyn/content/article/2005/06/22/AR2005062202305.html" target="pentagonbeingbitches">Pentagon Creates Massive Student Database</a><br>
-• <a href="http://www.msnbc.msn.com/id/8331097/" target="byehomehellowalmart">Private Business &gt; Private Home</a><br>
+&#8226; <a href="http://www.boingboing.net/2005/06/22/rottencom_our_gaping.html" target="nomorepr0n">US Code: Title 18: Section 2257</a><br>
+&#8226; <a href="http://www.washingtonpost.com/wp-dyn/content/article/2005/06/22/AR2005062202305.html" target="pentagonbeingbitches">Pentagon Creates Massive Student Database</a><br>
+&#8226; <a href="http://www.msnbc.msn.com/id/8331097/" target="byehomehellowalmart">Private Business &gt; Private Home</a><br>
 <br>
 I feel...violated...
 <br><br>
--- media.php
+++ media.php
@@ -12,16 +12,16 @@ include('./src/headTemplate.inc');
 <p>Yes, a media section.  How fun.  However, it seems that there are many kinds of media.  Oh dear, what to do.  Just throw it all into one page?  What lack of organization.  No.  We musn't.  But then, how?  A page for all?  Nay.  Too many, too much clutter.  Oh my, what is a boy to do?</p>
 
 <p>Images:<br>
-• <a href="/images/anti-newton.jpg" target="antiN">Screw Newton</a><br>
+&#8226; <a href="/images/anti-newton.jpg" target="antiN">Screw Newton</a><br>
 </p>
 
 <p>Video:</br>
-• <a href="/video/sweet.html" target="sweet">Sweet</a> - By Scott Hutchens<br>
-• <a href="/video/conan.html" target="aids">Conan Walker Clip - AIDS</a><br>
+&#8226; <a href="/video/sweet.html" target="sweet">Sweet</a> - By Scott Hutchens<br>
+&#8226; <a href="/video/conan.html" target="aids">Conan Walker Clip - AIDS</a><br>
 </p>
 
 <p>Flash:<br>
-• <a href="/video/Super Mario Physics.swf" target="mariophy">Mario Physics</a> - Obviously not by me.  Look at the credits for...credit.<br>
+&#8226; <a href="/video/Super Mario Physics.swf" target="mariophy">Mario Physics</a> - Obviously not by me.  Look at the credits for...credit.<br>
 </p>
 
 And that's pretty much the media page for now.  What fun!  I didn't have anything to do with more than half the shit in here!
